b1cb6767d0c53e3aabc7097e3cda1ca66b7c2807
Gentianlleshi <33518178+Gentianlleshi@users.noreply.github.com>
2024-03-05 16:50:32 +0100

    update types for useStore

--- src/app/stores/useStore.ts
+++ src/app/stores/useStore.ts
@@ -1,7 +1,15 @@
 import {create} from 'zustand'
 
-export const useStore = create((set) => ({
+// Define a state interface
+interface StoreState {
+  postsRefreshTrigger: number;
+  triggerPostsRefresh: () => void;
+}
+
+// Use the interface in the create() method to enforce type safety
+export const useStore = create<StoreState>((set) => ({
   postsRefreshTrigger: 0,
   triggerPostsRefresh: () => set((state) => ({ postsRefreshTrigger: state.postsRefreshTrigger + 1 })),
-}))
+}));
+
 export default useStore;
